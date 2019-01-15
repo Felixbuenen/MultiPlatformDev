7c4efd02179ef6f7d949072250967e1d0df94d99
Felix Buenen <felixbuenen@hotmail.com>
2019-01-15 11:03:36 +0100

    controls info UI

---
+++ Assets/Scripts/UI/ControlsInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlsInput : MonoBehaviour
+{
+
+	IInputSystem input;
+	SceneLoader menuLoader;
+
+	// Use this for initialization
+	void Start()
+	{
+		ServiceManager.Singleton.RequestService<IInputSystem>(out input);
+		ServiceManager.Singleton.RequestService<SceneLoader>(out menuLoader);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (input.UIInput.GetBackButton())
+		{
+			LoadMenu();
+		}
+	}
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadSceneAsync(menuLoader.MenuName);
+	}
+}
